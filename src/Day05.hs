c38c241dbe1b359bd9b7e351db6df5c65571a539
Mikko Matilainen <mikkom@iki.fi>
2021-12-05 13:09:52 +0200

    Everything is better with monoids

--- src/Day05.hs
+++ src/Day05.hs
@@ -5,8 +5,9 @@ module Day05 where
 import Control.Arrow ((&&&))
 import Data.List.Split (splitOn)
 import qualified Data.Map as M
+import Data.Monoid (Sum (Sum))
 
-type Point = (Int, Int)
+type Point = (Sum Int, Sum Int)
 
 type Line = (Point, Point)
 
@@ -25,20 +26,20 @@ countOverlaps :: [PointMap] -> Int
 countOverlaps = M.size . M.filter (> 1) . M.unionsWith (+)
 
 toPointMap :: Bool -> Line -> PointMap
-toPointMap skipDiagonal = M.fromList . (map (,1) . newPoints)
+toPointMap skipDiagonal = M.fromList . (map (,1) . points)
   where
-    newPoints ((x1, y1), (x2, y2))
-      | x1 == x2 = map (x1,) ys
-      | y1 == y2 = map (,y1) xs
-      | skipDiagonal = []
-      | (x1 < x2) == (y1 < y2) = zip xs ys
-      | otherwise = zip xs (reverse ys)
+    points (p1, p2)
+      | skipDiagonal && dx /= 0 && dy /= 0 = []
+      | otherwise = range (<> (dx, dy)) p1 p2
       where
-        xs = [min x1 x2 .. max x1 x2]
-        ys = [min y1 y2 .. max y1 y2]
+        (dx, dy) = (delta fst, delta snd)
+        delta f = signum $ f p2 - f p1
 
 parseLine :: String -> Line
 parseLine = (&&&) (parsePoint . head) (parsePoint . last) . words
   where
-    parsePoint = (&&&) (read . head . split) (read . last . split)
-    split = splitOn ","
+    parsePoint = (&&&) (head . coords) (last . coords)
+    coords = map (Sum . read) . splitOn ","
+
+range :: Eq t => (t -> t) -> t -> t -> [t]
+range step start end = takeWhile (/= step end) $ iterate step start
