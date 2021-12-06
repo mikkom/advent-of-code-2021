33b046bbad27642cbfc995c0db9e0a3b982f6217
Mikko Matilainen <mikkom@iki.fi>
2021-12-06 12:07:18 +0200

    Toying with groups

--- src/Day05.hs
+++ src/Day05.hs
@@ -2,11 +2,12 @@
 
 module Day05 where
 
+import Data.Group ((~~))
 import Data.List.Split (splitOn)
 import qualified Data.Map.Monoidal as M
-import Data.Monoid (Sum)
+import Data.Monoid (Sum (Sum))
 
-type Point = (Int, Int)
+type Point = (Sum Int, Sum Int)
 
 data Line = Line Point Point deriving (Eq, Show)
 
@@ -27,8 +28,8 @@ countOverlaps = M.size . M.filter (> 1) . mconcat
 toPointMap :: Line -> PointMap
 toPointMap = M.fromList . (map (,1) . points)
   where
-    points (Line (x1, y1) (x2, y2)) = zip (range x1 x2) (range y1 y2)
-    range a b = [a, a + signum (b - a) .. b]
+    points (Line p1 p2) = range p1 (mapPair signum (p2 ~~ p1)) p2
+    mapPair f (x, y) = (f x, f y)
 
 isStraight :: Line -> Bool
 isStraight (Line (x1, y1) (x2, y2)) = x1 == x2 || y1 == y2
@@ -36,6 +37,9 @@ isStraight (Line (x1, y1) (x2, y2)) = x1 == x2 || y1 == y2
 parseLine :: String -> Line
 parseLine = uncurry Line . toPair . map toPoint . splitOn " -> "
   where
-    toPoint = toPair . map read . splitOn ","
+    toPoint = toPair . map (Sum . read) . splitOn ","
     toPair [x, y] = (x, y)
     toPair _ = error "Invalid input"
+
+range :: (Eq m, Monoid m) => m -> m -> m -> [m]
+range start step end = takeWhile (/= end <> step) $ iterate (<> step) start
