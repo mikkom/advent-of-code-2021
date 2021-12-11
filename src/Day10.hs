933d4e96e6931fc1f0ff7a33be8c1037fe29e36f
Mikko Matilainen <mikkom@iki.fi>
2021-12-11 12:10:28 +0200

    Day10 cleanup

--- src/Day10.hs
+++ src/Day10.hs
@@ -3,34 +3,32 @@ module Day10 where
 import Control.Monad (foldM)
 import Data.Either (lefts, rights)
 import Data.List (elemIndex, foldl', sort)
-import Data.Maybe (fromJust)
+import Data.Maybe (listToMaybe, mapMaybe)
 
 main :: IO ()
-main = interact (unlines . sequence [part1, part2] . lines)
+main = interact (unlines . sequence [part1, part2] . map parse . lines)
 
-part1, part2 :: [String] -> String
-part1 = ("Part 1: " ++) . show . sum . map score . lefts . map parse
-part2 = ("Part 2: " ++) . show . middle . map score' . rights . map parse
+part1, part2 :: [Either Char String] -> String
+part1 = ("Part 1: " ++) . show . sum . mapMaybe score . lefts
+part2 = ("Part 2: " ++) . show . middle . sort . map score' . rights
 
 parse :: String -> Either Char String
 parse = foldM f []
   where
     f stack c
       | c `elem` open = Right (c : stack)
-      | take 1 stack == [matchingOpen c] = Right (tail stack)
+      | listToMaybe stack == matchingOpen c = Right (tail stack)
       | otherwise = Left c
-    matchingOpen c = fromJust $ lookup c $ zip closed open
+    matchingOpen = (`lookup` zip closed open)
 
-score :: Char -> Int
-score c = fromJust $ lookup c $ zip closed [3, 57, 1197, 25137]
+score :: Char -> Maybe Int
+score = (`lookup` zip closed [3, 57, 1197, 25137])
 
 score' :: String -> Int
-score' = foldl' (\acc c -> 5 * acc + scoreCh c) 0
-  where
-    scoreCh c = succ . fromJust $ elemIndex c open
+score' = foldl' (\acc i -> 5 * acc + i + 1) 0 . mapMaybe (`elemIndex` open)
 
-middle :: Ord a => [a] -> a
-middle xs = sort xs !! (length xs `div` 2)
+middle :: [a] -> a
+middle xs = xs !! (length xs `div` 2)
 
 open, closed :: String
 (open, closed) = ("([{<", ")]}>")
