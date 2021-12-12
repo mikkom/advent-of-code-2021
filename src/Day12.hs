abb3b2e88018cec1f9180a81e0aa56cd98d91d5a
Mikko Matilainen <mikkom@iki.fi>
2021-12-12 18:39:44 +0200

    Refactor to use same function in both parts

--- src/Day12.hs
+++ src/Day12.hs
@@ -3,8 +3,7 @@ module Day12 where
 import Data.Char (isLower)
 import Data.List (foldl')
 import Data.List.Split (splitOn)
-import Data.Map (Map, (!))
-import qualified Data.Map as M
+import Data.Map (Map, empty, insertWith, (!))
 import Data.Set (Set)
 import qualified Data.Set as S
 
@@ -16,38 +15,26 @@ main :: IO ()
 main = interact (unlines . sequence [part1, part2] . parse . lines)
 
 part1, part2 :: Input -> String
-part1 = ("Part 1: " ++) . show . length . paths
-part2 = ("Part 2: " ++) . show . length . paths'
-
-paths :: Input -> [[Cave]]
-paths input = findPaths input S.empty [] "start"
-
-paths' :: Input -> Set [Cave]
-paths' input = S.fromList $ findPaths' input S.empty Nothing [] "start"
+part1 = ("Part 1: " ++) . show . length . paths (Just "start")
+part2 = ("Part 2: " ++) . show . length . paths Nothing
 
 parse :: [String] -> Input
-parse = foldl' (\acc (k, v) -> M.insertWith (++) k [v] acc) M.empty . (>>= connection)
+parse = foldl' (\m (k, v) -> insertWith (++) k [v] m) empty . (>>= connection)
   where
     connection = listToPairs . splitOn "-"
     listToPairs [x, y] = [(x, y), (y, x)]
     listToPairs _ = error "Invalid input"
 
-findPaths :: Input -> Set Cave -> [Cave] -> Cave -> [[Cave]]
-findPaths _ _ path "end" = [reverse ("end" : path)]
-findPaths input visited path cave =
-  concatMap (findPaths input visited' (cave : path)) nextCaves
+paths :: Maybe Cave -> Input -> Set [Cave]
+paths twice input = S.fromList $ findPaths S.empty twice [] "start"
   where
-    nextCaves = filter (\c -> not $ S.member c visited) (input ! cave)
-    visited' = if all isLower cave then S.insert cave visited else visited
-
-findPaths' :: Input -> Set Cave -> Maybe Cave -> [Cave] -> Cave -> [[Cave]]
-findPaths' _ _ _ path "end" = [reverse ("end" : path)]
-findPaths' input visited twice path cave = case (twice, cave) of
-  (_, "start") -> concatMap (findPaths' input visited' twice (cave : path)) nextCaves
-  (Nothing, _) ->
-    concatMap (findPaths' input visited' Nothing (cave : path)) nextCaves
-      ++ concatMap (findPaths' input visited (Just cave) (cave : path)) nextCaves
-  _ -> concatMap (findPaths' input visited' twice (cave : path)) nextCaves
-  where
-    nextCaves = filter (\c -> not $ S.member c visited) (input ! cave)
-    visited' = if all isLower cave then S.insert cave visited else visited
+    findPaths _ _ path "end" = [reverse ("end" : path)]
+    findPaths visited twice path cave = case (twice, cave) of
+      (_, "start") -> recurse visited' twice
+      (Just _, _) -> recurse visited' twice
+      _ -> recurse visited' Nothing ++ recurse visited (Just cave)
+      where
+        nextCaves = filter (\c -> not $ S.member c visited) (input ! cave)
+        visited' = if all isLower cave then S.insert cave visited else visited
+        recurse visited twice =
+          concatMap (findPaths visited twice (cave : path)) nextCaves
