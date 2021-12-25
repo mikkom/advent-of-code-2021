0430cb2209b14b5e74e7f96a869f089e85ffd206
Mikko Matilainen <mikkom@iki.fi>
2021-12-25 15:30:02 +0200

    Cleanup

--- src/Day25.hs
+++ src/Day25.hs
@@ -1,10 +1,7 @@
-{-# LANGUAGE TupleSections #-}
-
 module Day25 where
 
 import Control.Arrow ((&&&))
-import Data.Bifunctor (first)
-import Data.List (foldl', intercalate)
+import Data.Bifunctor (Bifunctor (bimap, first, second))
 import Data.List.Split (splitOn)
 import Data.Map (Map, (!))
 import qualified Data.Map as M
@@ -13,48 +10,36 @@ import Data.Maybe (catMaybes)
 
 type Point = (Int, Int)
 
-data SeaCucumber = East | South deriving (Eq, Show)
+data Direction = East | South deriving (Eq, Show)
 
-type Input = (Map Point SeaCucumber, (Int, Int))
-
-todo = error "TODO"
+type Input = (Map Point Direction, (Int, Int))
 
 main :: IO ()
 main = interact (unlines . sequence [part1, part2] . parse)
 
 part1, part2 :: Input -> String
--- part1 = ("Part 1: \n" ++) . intercalate "\n\n" . map prettyPrint . take 59 . iterate step
-part1 = ("Part 1: " ++) . show . succ . length . whileDiffer . iterate step
+part1 = ("Part 1: " ++) . show . succ . length . takeUntilDuplicate . iterate step
 part2 = ("Part 2: " ++) . show . const ""
 
-firstDuplicate :: (Eq a) => [a] -> a
-firstDuplicate = fst . head . dropWhile (uncurry (/=)) . pairs
+takeUntilDuplicate :: (Eq a) => [a] -> [a]
+takeUntilDuplicate = map fst . takeWhile (uncurry (/=)) . pairs
   where
     pairs xs = zip xs (tail xs)
-
-whileDiffer :: (Eq a) => [a] -> [a]
-whileDiffer = map fst . takeWhile (uncurry (/=)) . pairs
-  where
-    pairs xs = zip xs (tail xs)
-
-stepEast :: Input -> Input
-stepEast (m, (rows, cols)) = (foldr move m (movers m), (rows, cols))
-  where
-    movers = filter hasSpace . M.keys . M.filter (== East)
-    move pos = M.insert (nextPos pos) East . M.delete pos
-    nextPos (i, j) = (i, (j + 1) `mod` cols)
-    hasSpace pos = not $ M.member (nextPos pos) m
-
-stepSouth :: Input -> Input
-stepSouth (m, (rows, cols)) = (foldr move m (movers m), (rows, cols))
-  where
-    movers = filter hasSpace . M.keys . M.filter (== South)
-    move pos = M.insert (nextPos pos) South . M.delete pos
-    nextPos (i, j) = ((i + 1) `mod` rows, j)
-    hasSpace pos = not $ M.member (nextPos pos) m
 
 step :: Input -> Input
-step = stepSouth . stepEast
+step input@(m, (rows, cols)) = first go input
+  where
+    go = stepDir South nextSouth . stepDir East nextEast
+    nextEast = wrap . second succ
+    nextSouth = wrap . first succ
+    wrap = bimap (`mod` rows) (`mod` cols)
+
+stepDir :: (Ord k, Eq a) => a -> (k -> k) -> Map k a -> Map k a
+stepDir dir nextPos m = foldr move m (movers m)
+  where
+    movers = filter hasSpace . M.keys . M.filter (== dir)
+    move pos = M.insert (nextPos pos) dir . M.delete pos
+    hasSpace pos = not $ M.member (nextPos pos) m
 
 parse :: String -> Input
 parse = (&&&) parseRows getSize . splitOn "\n"
@@ -66,11 +51,3 @@ parse = (&&&) parseRows getSize . splitOn "\n"
     parseChar '>' = Just East
     parseChar 'v' = Just South
     parseChar _ = Nothing
-
-prettyPrint :: Input -> String
-prettyPrint (m, (rows, cols)) = intercalate "\n" $ map (printRow m) [0 .. rows - 1]
-  where
-    printRow m i = map (print . (`M.lookup` m) . (i,)) [0 .. cols - 1]
-    print (Just East) = '>'
-    print (Just South) = 'v'
-    print Nothing = '.'
